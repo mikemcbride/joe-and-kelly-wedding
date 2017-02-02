d12440ad473e645255883d3f5b6cf1da55eee1b2
Mike McBride <mmcbride1007@gmail.com>
2017-02-01 23:47:08 -0600

    get started

--- dev_assets/js/main.js
+++ dev_assets/js/main.js
@@ -1,2 +1,20 @@
-// add scripts here!
-console.log('Hello, beautiful! You can modify scripts in dev_assets/js/');
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('rick-roll', {
+      playerVars: { 'autoplay': 1, 'controls': 0 },
+      events: {
+        'onReady': onPlayerReady
+      }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.setVolume(100);
+  event.target.playVideo();
+}
