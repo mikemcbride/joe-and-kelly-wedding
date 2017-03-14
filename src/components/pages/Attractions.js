41f91feac097c8418ef47da56d2bcbf8a6d60347
Mike McBride <mmcbride1007@gmail.com>
2017-03-13 22:12:47 -0500

    adds data to registries page and attractions page

--- src/components/pages/Attractions.js
+++ src/components/pages/Attractions.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import Page from '../Page'
+import attractions from '../../data/attractions'
 
 export const Attractions = () => (
   <Page>
-    <p>Attractions</p>
+    <p className="mb4">Looking for some fun things to do around KC? Here’s what the couple suggests:</p>
+    <div className="ba b--white-10 br1 mb4">
+      <h3 className="f5 f4-ns b ph2 ph3-ns pv3 bb b--white-10 ttu gold mv0">Joe's Picks</h3>
+      <div className="ph2 pv3 ph3-ns">
+        <p className="mb3"><strong className="gold ttu">BBQ:</strong> {attractions.joe.bbq}</p>
+        <p className="mb3"><strong className="gold ttu">Fun:</strong> {attractions.joe.fun}</p>
+        <p><strong className="gold ttu">Drink:</strong> {attractions.joe.drink}</p>
+      </div>
+    </div>
+    <div className="ba b--white-10 br1 mb4">
+      <h3 className="f5 f4-ns b ph2 ph3-ns pv3 bb b--white-10 ttu gold mv0">Kelly's Picks</h3>
+      <div className="ph2 pv3 ph3-ns">
+        <p className="mb3"><strong className="gold ttu">BBQ:</strong> {attractions.kelly.bbq}</p>
+        <p className="mb3"><strong className="gold ttu">Fun:</strong> {attractions.kelly.fun}</p>
+        <p><strong className="gold ttu">Drink:</strong> {attractions.kelly.drink}</p>
+      </div>
+    </div>
   </Page>
 )
---
+++ src/data/attractions.js
@@ -0,0 +1,14 @@
+const attractions = {
+  "joe": {
+    "bbq": "No trip is complete without some BBQ. Head to the iconic gas station for the best in town – Joe's KC (formerly Oklahoma Joe's). Pro tip – call ahead, skip the line. And order the Z-Man with pork.",
+    "fun": "Check out the College Basketball Experience and Hall of Fame at the Sprint Center. Be sure to bring your basketball shorts – this experience is interactive. I took my brothers and taught them all how to properly dunk.",
+    "drink": "Up/Down – like Chuck E. Cheese, but for adults. This bar was tons of old school video games, including X-Men, Mortal Kombat, NBA Jam, NFL Blitz, the Simpson's, and more."
+  },
+  "kelly": {
+    "bbq": " I already called dibs on Joe’s. You are a q-39 girl anyway.",
+    "fun": "no idea. Maybe check out the Christmas lights on the plaza or Union Station cause it will be that time of year.",
+    "drink": "my guess is Julep or Green Lady Lounge. You should pick your bar though, Kelly’s."
+  }
+}
+
+export default attractions
