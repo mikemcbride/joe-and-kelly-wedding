91e14240c8c2bcd0950654126dc29b494871c808
Mike McBride <mmcbride1007@gmail.com>
2017-07-27 10:42:21 -0500

    remove dummy text

--- src/components/pages/Party.js
+++ src/components/pages/Party.js
@@ -5,8 +5,6 @@ import party from '../../data/party.js'
 
 export const Party = () => (
   <Page>
-    <p className="lh-copy mb4">Joe and Kelly have friends. Well, Kelly does. Joe has his brothers (and Mike who doesn't really count). Either way, their names are listed below for you to read.</p>
-    
     <section className="mb4 mb5-ns">
       <h1 className="f3 b ttu gold mt0 pb2 bb bw1">The Ladies</h1>
       <div className="flex flex-wrap">
