64a561163733a5707f170bac21c8a800c441e415
Mike McBride <mmcbride1007@gmail.com>
2017-08-08 21:03:02 -0500

    update banner photo

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Page from '../components/Page'
 
 export default () => (
   <Page activeNavItem="home">
-    <div className="center mw7 relative">
-      <img src='/static/img/banner.jpg' alt="Joe and Kelly" className="grayscale z-0" />
-      <span className="absolute top-0 left-0 right-0 tc pv2 pv3-ns pv4-l z-1 gold f4 f3-ns f2-l fw7 bg-black-80">
-        <div>November 25, 2017</div>
-      </span>
+    <div className="center mw7 relative w-100">
+      <img src='/static/img/banner.jpg' alt="Joe and Kelly" className="dib w-100 z-0 grayscale" />
+      <span className="absolute bottom-1 left-0 right-0 tc pv2 pv3-ns z-1 gold f4 f3-ns f2-l fw7 bg-black-80">November 25, 2017</span>
     </div>
   </Page>
 )
