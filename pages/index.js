387e11cd7c3e76e0500b2ee6ca415ca3cbff0171
Mike McBride <mmcbride1007@gmail.com>
2017-09-17 14:35:30 -0500

    update banner image

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Page from '../components/Page'
 
 export default () => (
   <Page activeNavItem="home">
-    <div className="center mw7 relative w-100">
-      <img src='/static/img/banner.jpg' alt="Joe and Kelly" className="dib w-100 z-0 grayscale" />
-      <span className="absolute bottom-1 left-0 right-0 tc pv2 pv3-ns z-1 gold f4 f3-ns f2-l fw7 bg-black-80">November 25, 2017</span>
+    <div className="center mw7 relative w-100 tc">
+      <img src='/static/img/banner.jpg' alt="Joe and Kelly" className="w-80-ns dib z-0 grayscale" />
+      <span className="absolute bottom-1 absolute-center w-100 w-80-ns tc pv2 pv3-ns z-1 gold f4 f3-ns f2-l fw7 bg-black-80">November 25, 2017</span>
     </div>
   </Page>
 )
--- pages/story.js
+++ pages/story.js
@@ -6,8 +6,8 @@ export default () => (
   <Page activeNavItem="story">
     <div className="lh-copy">
       <div className="mb4 cf center mw9">
-        <img src="/static/img/mizzou.jpg" className="w-50 fl pa2" />
-        <img src="/static/img/proposal.jpg" className="w-50 fl pa2 grayscale" />
+        <img src="/static/img/mizzou.jpg" className="w-100 w-50-ns fl pa2" />
+        <img src="/static/img/proposal.jpg" className="w-100 w-50-ns fl pa2 grayscale" />
       </div>
       <h3 className="f4 fl dib pa3 ba mr2 b ttu gold mb0 mt1">Joe</h3>
       <p className="mb4">{ story.joe }</p>
